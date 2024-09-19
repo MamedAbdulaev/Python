2c1d7725aa5300f67d4b030fc9d1173aab8238e1
MamedAbdulaev <156102043+MamedAbdulaev@users.noreply.github.com>
2024-09-19 17:47:37 +0300

    Add files via upload

---
+++ lesson_3_task_2.py
@@ -0,0 +1,15 @@
+from smartphone import Smartphone
+
+catalog = []
+
+phone_one = Smartphone("Honor","70","+49415191959")
+phone_two = Smartphone("Xiomi","Redmi Note 12 Pro","+49415191971")
+phone_three = Smartphone("Apple","15 Pro Max","+49415191975")
+phone_four = Smartphone("OnePlus","Nord N30","+49415191979")
+phone_five = Smartphone("Samsung ","Galaxy S23 Ultra","+49415191990")
+
+catalog.append(phone_one)
+catalog.append(phone_two)
+catalog.append(phone_three)
+catalog.append(phone_four)
+catalog.append(phone_five)

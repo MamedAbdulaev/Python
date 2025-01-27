21c69f5801db6734fe7e8f366171f7f941958dc0
MamedAbdulaev <156102043+MamedAbdulaev@users.noreply.github.com>
2025-01-27 14:49:16 +0000

    сделал все задания

--- 03_lesson/lesson_3_task_2.py
+++ 03_lesson/lesson_3_task_2.py
@@ -1,15 +1,6 @@
 from smartphone import Smartphone
-
-catalog = []
-
-phone_one = Smartphone("Honor","70","+49415191959")
-phone_two = Smartphone("Xiomi","Redmi Note 12 Pro","+49415191971")
-phone_three = Smartphone("Apple","15 Pro Max","+49415191975")
-phone_four = Smartphone("OnePlus","Nord N30","+49415191979")
-phone_five = Smartphone("Samsung ","Galaxy S23 Ultra","+49415191990")
-
-catalog.append(phone_one)
-catalog.append(phone_two)
-catalog.append(phone_three)
-catalog.append(phone_four)
-catalog.append(phone_five)
+catalog = [Smartphone ("Nokia","Fonarik", "9999999999"),
+            Smartphone ("Samsung", "Galaxy","91111111111"),
+            Smartphone ("iPhone","XR","98888888888")]
+for Smartphone in catalog:
+    print (Smartphone.vendor, "-", Smartphone.model,".",Smartphone.num)

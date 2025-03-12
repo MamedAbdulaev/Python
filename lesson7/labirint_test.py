795f6ce3bf1f97fa3fadbde9e7c3c2cda52bb350
MamedAbdulaev <m.abdulaev@kazanexpress.tech>
2025-03-12 16:26:26 +0300

    7-й урок + реструктуризация

--- lesson7/labirint_test.py
+++ lesson7/labirint_test.py
@@ -2,3 +2,21 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from time import sleep
+
+def test_card_counter():
+    browser = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()))
+
+    # Перейти на сайт «Лабиринта»
+    browser.get("https://www.labirint.ru/")
+    browser.implicitly_wait(4)
+    browser.maximize_window()
+    browser.add_cookie(cookie)
+    sleep(5)
+    # Найти все книги по слову Python
+    # Переключиться на таблицу
+    # Добавить все книги в корзину и посчитать
+    # Перейти в корзину
+    # Проверить счетчик товаров. Должен быть равным числу нажатий
+    browser.quit()
